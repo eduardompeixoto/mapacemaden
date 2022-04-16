8c924cff1cb852a0eb52a93237c3fd64fbfb1604
eduardompeixoto <46012106+eduardompeixoto@users.noreply.github.com>
2022-04-16 02:37:22 -0300

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("tmap","cartogram")
+my_packages = c("rvest","flextable","reshape","dplyr")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
